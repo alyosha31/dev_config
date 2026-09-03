99641a5d1e93fcc84dcdd682cc97f57a429d6948
alyosha31 <mohitsinghguleria313@gmail.com>
2026-09-03 16:47:30 +0530

    Add Diffview to Neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -48,6 +48,27 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 		},
 	},
+	{
+		"sindrets/diffview.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewToggleFiles",
+			"DiffviewFocusFiles",
+			"DiffviewRefresh",
+			"DiffviewFileHistory",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Open Git diff" },
+			{ "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", desc = "Current file Git history" },
+			{ "<leader>gH", "<cmd>DiffviewFileHistory<CR>", desc = "Repository Git history" },
+			{ "<leader>gq", "<cmd>DiffviewClose<CR>", desc = "Close Git diff" },
+		},
+		opts = {},
+	},
 
 	-- search / picker
 	{
