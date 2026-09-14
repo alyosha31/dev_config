1d163580aefea9b7c99d51201cff2f163cf28684
Mohit Guleria <mg@srv1979359.hstgr.cloud>
2026-09-14 15:21:06 +0000

    Configure OSC 52 clipboard

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -27,6 +27,17 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.cursorline = true
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 vim.opt.mouse = "a"
 vim.opt.swapfile = false
 vim.opt.backup = false
